011e377dce8b19a86de024e092c39c51157d72b3
willfrw <william.wang_dev@outlook.com>
2025-01-18 17:43:28 -0300

    First Commit

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,45 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = @"Sejam bem-vindos ao Screen Sound";
+
+void ExibirMensagemDeBoasVindas(string mensagem) 
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagem);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exebir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case -1: Console.WriteLine("Finalizado :)");
+            break;
+        default: Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+ExibirMensagemDeBoasVindas(mensagemDeBoasVindas);
+ExibirOpcoesDoMenu();
